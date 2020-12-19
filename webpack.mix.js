b3a9073db65624d861724b920a6dacd2c07845bd
Mdsujansarkar <sujansarkar786@gmail.com>
2020-12-19 23:49:33 +0600

    tailwind css

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,6 @@
 const mix = require('laravel-mix');
 
+
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -11,5 +12,9 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+
+   mix.js("resources/js/app.js", "public/js")
+   .postCss("resources/css/app.css", "public/css", [
+
+    require("tailwindcss"),
+   ]);
