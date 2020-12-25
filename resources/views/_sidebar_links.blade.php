d8430ab1975bb4547c64da994d41108c87ad6e03
Mdsujansarkar <sujansarkar786@gmail.com>
2020-12-25 12:27:54 +0600

    hello

---
+++ resources/views/_sidebar_links.blade.php
@@ -0,0 +1,42 @@
+<ul>
+	<li>
+		<a 
+		class="font-bold text-lg mb-4 block" 
+		href="/"
+		>
+		Home
+		</a>
+	</li>
+	<li>
+		<a 
+		class="font-bold text-lg mb-4 block" 
+		href="/explore"
+		>
+		Explore
+		</a>
+	</li>
+	<li>
+		<a 
+		class="font-bold text-lg mb-4 block" 
+		href="/notification"
+		>
+		Notifications
+		</a>
+	</li>
+	<li>
+		<a 
+		class="font-bold text-lg mb-4 block" 
+		href="/message"
+		>
+		message
+		</a>
+	</li>
+	<li>
+		<a 
+		class="font-bold text-lg mb-4 block" 
+		href="/explore"
+		>
+		Explore
+		</a>
+	</li>
+</ul>
--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@
 
 Route::get('/', function () {
 
-    return view('layouts.app');
+    return view('home');
 });
 
 Auth::routes();
